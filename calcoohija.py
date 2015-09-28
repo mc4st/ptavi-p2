4d829c1a355a5db61bb66d39b80536a1f953ed10
marta castejon cachero <mcastejo@zeta40.aulas.gsyc.es>
2015-09-28 16:43:05 +0200

    Añadido Multiplucacion y Division

--- calcoohija.py
+++ calcoohija.py
@@ -9,6 +9,12 @@ class Calculadora():
     def minus(self, op1, op2):
         return op1 - op2
 
+class CalculadoraHija(Calculadora):
+    def mult(self, op1, op2):
+        return op1 * op2
+    def div(self, op1, op2):
+        return op1 / op2
+
 if __name__ == "__main__":
     try:
         op1 = int(sys.argv[1])
@@ -16,11 +22,17 @@ if __name__ == "__main__":
     except ValueError:
         sys.exit("Error: Non numerical parameters")
 
-    calculadora = Calculadora()
+    calculadora = CalculadoraHija()
 
     if sys.argv[2] == "suma":
         result = calculadora.plus(op1, op2)
     elif sys.argv[2] == "resta":
         result = calculadora.minus(op1, op2)
-
+    elif sys.argv[2] == "multiplicacion":
+        result = calculadora.mult(op1, op2)
+    elif sys.argv[2] == "division":
+        if op2 != 0:
+            result = calculadora.div(op1, op2)
+        else:
+            sys.exit('Division by zero is not allowed')
     print(result)
