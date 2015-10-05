e44f87cc10f58efd9214ce7e8f7d6a0a0cd839b7
mc4st <C4st3j0n>
2015-10-05 22:40:43 +0200

    Final

--- calcplus.py
+++ calcplus.py
@@ -4,36 +4,17 @@
 import sys
 import calcoohija
 
-fich = open(sys.argv[1], "r")
-lineas = fich.readlines()
+if __name__ == "__main__":
+    fich = open(sys.argv[1], "r")
+    lineas = fich.readlines()
+    calc = calcoohija.CalculadoraHija()
 
-calc = calcoohija.CalculadoraHija()
-
-try:
     for linea in lineas:
         datos = linea.split(',')
         operacion = datos[0]
-        if operacion == "suma":
-            suma = 0
-            for operandos in datos[1:]:
-                suma = calc.plus(suma, int(operandos))
-        if operacion == "resta":
-            rest = int(datos[1])
+        diccoperandos = {"suma": calc.plus, "resta": calc.minus, "multiplica": calc.mult, "divide": calc.div}
+        if operacion in ["suma", "resta", "multiplica", "divide"]:
+            solucion = datos[1]
             for operandos in datos[2:]:
-                rest = calc.minus(rest, int(operandos))
-        if operacion == "multiplica":
-            mult = int(datos[1])
-            for operandos in datos[2:]:
-                mult = calc.mult(mult, int(operandos))
-        if operacion == "divide":
-            div = int(datos[1])
-            for operandos in datos[2:]:
-                div = calc.div(div, int(operandos))
-
-    print(suma)
-    print(rest)
-    print(mult)
-    print(div)
-except ValueError:
-    sys.exit("Error: Non numerical parameters")
-
+                solucion = diccoperandos[operacion](solucion, operandos)
+            print(solucion)
