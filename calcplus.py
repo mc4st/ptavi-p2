547e30576132afbbc829629250305b260241e776
marta castejon cachero <mcastejo@zeta40.aulas.gsyc.es>
2015-10-07 10:41:44 +0200

    Pasado check

--- calcplus.py
+++ calcplus.py
@@ -14,7 +14,7 @@ if __name__ == "__main__":
         datos = linea.split(',')
         operacion = datos[0]
         diccoperandos = {"suma": calc.plus, "resta": calc.minus, 
-                         "multiplica": calc.mult,"divide": calc.div}
+                         "multiplica": calc.mult, "divide": calc.div}
         if operacion in ["suma", "resta", "multiplica", "divide"]:
             solucion = datos[1]
             for operandos in datos[2:]:
